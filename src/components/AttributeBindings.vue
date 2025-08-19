ac87bae818f213ab44c1aaa6115c8f05abf3573c
rwrw96 <kazuki137@gmail.com>
2025-08-19 09:11:35 +0900

    実装例: Attribute Bindings

---
+++ src/components/AttributeBindings.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const titleText = ref('titleだよ')
+const isRed = ref(true)
+const color = ref('green')
+function toggleColor() {
+    color.value = color.value === 'blue' ? 'green' : 'blue'
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <p :title="titleText">Hover your mouse over me for a few seconds to see my dynamically bound title!</p>
+        <p :class="{ 'red-content' : isRed }" @click="isRed = !isRed">This should be red... but click me to toggle it.</p>
+        <p :style="{ color }" @click="toggleColor">This should be green, and should toggle between green and blue on click.</p>
+    </div>
+</template>
+
+<style>
+.red-content {
+    color: red;
+}
+.green-content {
+    color: green;
+}
+.blue-content {
+    color: blue;
+}
+</style>
--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import HandingUserInput from '../components/HandlingUserInput.vue';
+import AttributeBindings from '../components/AttributeBindings.vue';
 
 const isHideHandingUserInput = ref(false)
+const isAttributeBindings = ref(false)
 
 function toggleHideHandingUserInput() {
     isHideHandingUserInput.value = !isHideHandingUserInput.value
+}
+function toggleAttributeBindings() {
+    isAttributeBindings.value = !isAttributeBindings.value
 }
 </script>
 
 <template>
     <HandingUserInput v-if="isHideHandingUserInput"/>
+    <AttributeBindings v-if="isAttributeBindings"/>
     <div class="hide-button-wrapper">
-        <button class="" @click="toggleHideHandingUserInput()">HandingUserInput</button>
+        <button class="" @click="toggleHideHandingUserInput">HandingUserInput</button>
+        <button class="" @click="toggleAttributeBindings">Attribute Bindings</button>
     </div>
 </template>
 
