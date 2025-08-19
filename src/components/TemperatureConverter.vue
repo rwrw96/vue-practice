633a93c110fb14428f3e9b652ff9cb4f35828113
rwrw96 <kazuki137@gmail.com>
2025-08-19 22:20:25 +0900

    実装例: Temperature Converter

---
+++ src/components/TemperatureConverter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const celsius = ref(0)
+const fahrenheit = computed({
+    get: () => celsius.value*1.8+32,
+    set: (val) => celsius.value = (val-32)/1.8
+})
+</script>
+
+<template>
+    <div class="wraper">
+        <input type="number" v-model="celsius" />
+        Celsius =
+        <input type="number" v-model=fahrenheit />Fahrenheit
+    </div>
+</template>
--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -2,9 +2,11 @@
 import { ref } from 'vue';
 import HandingUserInput from '../components/HandlingUserInput.vue';
 import AttributeBindings from '../components/AttributeBindings.vue';
+import TemperatureConverter from '../components/TemperatureConverter.vue';
 
 const isHideHandingUserInput = ref(false)
 const isAttributeBindings = ref(false)
+const isTemperatureConverter = ref(false)
 
 function toggleHideHandingUserInput() {
     isHideHandingUserInput.value = !isHideHandingUserInput.value
@@ -12,14 +14,19 @@ function toggleHideHandingUserInput() {
 function toggleAttributeBindings() {
     isAttributeBindings.value = !isAttributeBindings.value
 }
+function toggleTemperatureConverter() {
+    isTemperatureConverter.value = !isTemperatureConverter.value
+}
 </script>
 
 <template>
     <HandingUserInput v-if="isHideHandingUserInput"/>
     <AttributeBindings v-if="isAttributeBindings"/>
+    <TemperatureConverter v-if="isTemperatureConverter"/>
     <div class="hide-button-wrapper">
         <button class="" @click="toggleHideHandingUserInput">HandingUserInput</button>
         <button class="" @click="toggleAttributeBindings">Attribute Bindings</button>
+        <button class="" @click="toggleTemperatureConverter">Temperature Converter</button>
     </div>
 </template>
 
